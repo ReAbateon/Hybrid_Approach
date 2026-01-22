47548a2c119cbfaa5cf5c399f49db213aa078e68
Lorenzo Abate <101709050+ReAbateon@users.noreply.github.com>
2026-01-22 15:25:57 +0100

    Update trainXGB.py

--- Code_Gen/Utils/trainXGB.py
+++ Code_Gen/Utils/trainXGB.py
@@ -1,5 +1,5 @@
 # 
-# Copyright (c) 2026 Lorenzo Abate.
+# Copyright (c) 2026 Lorenzo Abate <lorenzoabate510@gmail.com>.
 # 
 # This program is free software: you can redistribute it and/or modify  
 # it under the terms of the GNU General Public License as published by  
